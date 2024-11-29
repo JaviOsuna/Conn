19bfe66e4d333fa1f590c75fa9423121a93b66d3
Javier Osuna <javiosuna01@gmail.com>
2024-11-29 19:38:55 +0100

    Nueva implementacion con postgres

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Conn
 
 [![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+## Run Postgresql
+
+```sh
+docker run --name some-postgres -e POSTGRES_DB=recipes3 -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
+```
--- conn_postgressqp_orm.py
+++ conn_postgressqp_orm.py
@@ -8,26 +8,27 @@ from typing import Optional
 
 from sqlmodel import Session, Field, SQLModel, create_engine, select
 
-username = os.getenv("ORACLE_USER")
-password = os.getenv("ORACLE_PASSWORD")
-hostname = os.getenv("ORACLE_HOSTNAME")
-sid = os.getenv("ORACLE_SID")
+username = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+hostname = os.getenv("POSTGRES_HOSTNAME")
+port = os.getenv("POSTGRES_PORT")
+db_name = os.getenv("POSTGRES_DBNAME")
 
 
-class Recipe2(SQLModel, table=True):
+class Recipe3(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(max_length=30)
 
 
-oracle_url = f"oracle+oracledb://{username}:{password}@{hostname}/{sid}"
+oracle_url = f"postgresql+psycopg://{username}:{password}@{hostname}:{port}/{db_name}"
 engine = create_engine(oracle_url, echo=True)
 
 SQLModel.metadata.create_all(engine)
 
 
 def create_recipes():
-    recipe1 = Recipe2(id=10, name="Tarta de queso")
-    recipe2 = Recipe2(id=11, name="Tostada de trufa")
+    recipe1 = Recipe3(id=10, name="Tarta de queso")
+    recipe2 = Recipe3(id=11, name="Tostada de trufa")
 
     with Session(engine) as session:
         session.add(recipe1)
